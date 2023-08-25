418021b9c43b5fa0cc74ed9e8de8d66c0cfef1d8
vinkario <vinkario@DESKTOP-367A50O>
2023-08-25 15:38:00 -0300

    começo exercicio

---
+++ exercicio.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int questao;
+
+
+
+    printf("------------------------ Lista 02 - Condicionais ---------------------------");
+printf("QUESTAO 01 \t QUESTAO 02 \n");
+printf("QUESTAO 03 \t QUESTAO 04 \n");
+printf("QUESTAO 05 \t QUESTAO 06 \n");
+printf("QUESTAO 07 \t QUESTAO 08 \n");
+printf("QUESTAO 09 \t QUESTAO 10 \n");
+    return 0;
+}
